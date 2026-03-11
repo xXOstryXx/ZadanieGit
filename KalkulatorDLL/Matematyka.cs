f1e59ac3540d052e99fc0a4f1e90056bda03500e
xXOstryXx <niko762003@gmail.com>
2026-03-11 17:54:28 +0100

    kalkulator klasy

---
+++ KalkulatorDLL/Matematyka.cs
@@ -0,0 +1,27 @@
+﻿namespace KalkulatorDLL
+{
+    public class Matematyka
+    {
+        public double Dodawanie(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Odejmowanie(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mnozenie(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Dzielenie(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Nie można dzielić przez zero!");
+            return a / b;
+        }
+    }
+}
